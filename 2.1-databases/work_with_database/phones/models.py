6747a42d23084699dc2876299b46935e26b9eb41
Evgeniy Chausov <evgeniy@chausov.ru>
2023-10-10 10:52:45 +0300

    Done work_with_database

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=64, verbose_name='Модель телефона')
+    price = models.IntegerField(verbose_name='Цена')
+    image = models.CharField(max_length=200, verbose_name='Изображение')
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(blank=True, unique=True, verbose_name='URL')
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.template.defaultfilters import slugify
+
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +10,39 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort', 'name')
+    phone_objects = Phone.objects.all()
+    phone = []
+    for phones in phone_objects:
+        item = {
+            'name': phones.name,
+            'price': phones.price,
+            'image': phones.image,
+            'release_date': phones.release_date,
+            'lte_exists': phones.lte_exists,
+            'slug': slugify(phones.name)
+        }
+        phone.append(item)
+    if sort == 'min_price':
+        context = {'phones': sorted(phone, key=lambda x: x['price'])}
+    elif sort == 'max_price':
+        context = {'phones': sorted(phone, key=lambda x: -x['price'])}
+    else:
+        context = {'phones': sorted(phone, key=lambda x: x['name'])}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_object = Phone.objects.filter(slug=slug)
+    for item in phone_object:
+        phone = {
+            'name': item.name,
+            'price': item.price,
+            'image': item.image,
+            'release_date': item.release_date,
+            'lte_exists': item.lte_exists,
+            'slug': slugify(item.name)
+        }
+    context = {'phone': phone}
     return render(request, template, context)
