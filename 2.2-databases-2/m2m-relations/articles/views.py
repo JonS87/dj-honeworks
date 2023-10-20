a03ac3c08967b64877a809eb7e92b8fe24ec4bdd
Evgeniy Chausov <evgeniy@chausov.ru>
2023-10-20 15:28:19 +0300

    Done m2m-relations

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -4,11 +4,12 @@ from articles.models import Article
 
 
 def articles_list(request):
+    object_list = Article.objects.all()
     template = 'articles/news.html'
-    context = {}
+    context = {'object_list': object_list}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
+    #ordering = '-published_at'
 
     return render(request, template, context)
